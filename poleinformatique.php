2f8c1cd91e07fa8fd3422195099744be680907c4
QuentinPerret <quentinprrt33@gmail.com>
2022-05-15 16:58:11 +0200

    first commit info

--- poleinformatique.php
+++ poleinformatique.php
@@ -2,7 +2,7 @@
   <html>
 
   <?php
-    $pageTitle = "Ajout d'un film";
+    $pageTitle = "Pole Informatique";
     require_once "includes/head.php";
     ?>
 
@@ -11,6 +11,42 @@
         <?php require_once "includes/header.php"; ?>
 
         <div class="container-fluid">
+		<h1>Travail réalisé</h1>
+		<div class="row">
+			<div class="col-lg-12 col-md-12 col-sm-12 contenu">
+				<h3>Progression du projet</h3>
+				<p><b>Première partie : reprise du projet</b>
+				</p><ul>
+					<li>Le travail du pole informatique en octobre a commencé par une reprise du projet et des différents scripts laissé sur le wiki par le groupe d'étudiants de l'année précédente. Deux scripts ont été laissée : un en C et un en python. Les membres du pôle ayant déja fait du python. Nous avons décidé de nous concentré d'abord sur le scripts python lors de notre travail de reprise.</li>
+					<li>Les scripts laissées permettaient de transformer des valeurs héxadécimales contenus dans un fichier en des valuers décimales. C'est scripts ont pour objectif de transformé les valeurs envoyés par le capteur Végétal Signal afin de rendre leur traitement plus facile par la suite.</li>
+					<li>Néanmoins le code était très peu commenté et donc peu compréhensible, dans un soucis de partage nous avons décidé de le commenter plus précisement et de le documenté sur notre page wiki.</li>
+				</ul>
+				<b>Deuxème partie : traitement du signal</b>
+				<ul>
+					<li>Une fois le scrtipt python documenté nous avons souhaité ajouter au script python une partie de traitement du signal obtenu en étudiant sur moyenne au cours du temps mais aussi en décompant le sigal en série de Fourier.</li>
+					<li></li>
+					<li></li>
+				</ul>
+				<b>Pistes d'amélioration</b>
+				<ul>
+					<li>Une répartition et une planification des tâches de chacun en fonction de ses préférences et/ou compétences sur les deux projets.</li>
+					<li>Une meilleure mise en place du travail à domicile.</li>
+					<li>Nécessite un certain nombre de connaissances de bases, bien que nous ayons tout de même découvert de nombreux domaines.</li>
+					<li>Groupe mixte (ex : ENSC + Licence plus orientée informatique)</li>
+				</ul>
+				<p></p>
+				
+				<h3>Retours personnels</h3>
+				<ul><li><b>Romain : </b>Ce projet a permis à l’ensemble des membres du groupe de toucher à de nombreux domaines et à diversifier leur panel de compétences, et à permis de nous ouvrir à de nouveaux domaines. L’ensemble de l’équipe du Fablab a été d’une grande compréhension et patience, ce qui a accentué notre envie et notre plaisir à poursuivre le projet chaque semaine. Nous remercions plus particulièrement Pierre Grangé-Praderas, pour nous avoir transmis son savoir et nous avoir aidé dans la progression du projet.</li>
+				<li><b>Robin : </b>Ce projet transdisciplinaire a été l’occasion pour moi de m'enrichir autant d’un point de vue pédagogique que social. La complexité du projet au vu de mes anciennes années d’étude m’a permis de me former sur des sujets encore inconnus mais très intéressants: impression 3D, programmation Bash… A cette occasion j’ai également eu la chance de découvrir une structure comme le Fablab Coh@bit composée de personnes attentionnées prêtes à vous aider à n’importe quel moment. Le rendu de fin d’année n’est malheureusement pas à la hauteur de nos attentes, cependant nous avons apporté notre pierre à l’édifice quant à la progression de notre robot vers le soleil.</li>
+				<li><b>Brandon : </b>Au cours de ce projet, nous avons pu apprendre beaucoup de choses dans des domaines très variés (modélisation 3D avec FreeCad, impression 3D, commandes BASH, …). Ce fut donc une expérience très enrichissante. De surcroît, l’ambiance au sein du groupe était très agréable, de même qu’avec les divers membres du Fablab avec lesquels nous avons pu travailler. Leur pédagogie nous a permis d’avancer et de faire évoluer le projet malgré la présence de disciplines qui nous étaient inconnues. Les acquis pédagogiques furent donc majeurs. J’aurai néanmoins aimé avoir l’occasion d’avancer un petit peu plus sur ce projet, néanmoins les conditions sanitaires et la difficulté de la plupart des tâches nous ont beaucoup ralenties. Enfin, faisant moi même assez régulièrement des projets manuels sur mon temps libre, un lieu regorgeant d’outils comme le Fablab était naturellement une agréable découverte. </li>
+				<li><b>Lucas : </b>Pour moi le projet transdisciplinaire fut une expérience agréable. Il m’a permis de découvrir des structures différentes de celles auxquelles j’étais habitué. En effet, le Fablab est un lieu de partage de connaissances et d’entraide sur les projets qui est fort intéressant. Nous avons pu échanger avec des personnes de divers horizons, ce qui était enrichissant. Au niveau du projet en lui-même, j’ai acquis des nouvelles compétences et je me suis également rendu compte de l’importance de la méthodologie et de l’organisation du travail en groupe sur des projets conséquents et impliquant des acteurs extérieurs. </li></ul>
+				
+				<h3>Plan Vert</h3>
+				<p>Le Plan Vert doit recouvrir toutes les dimensions du développement durable c'est-à-dire par des aspects économiques, sociaux et environnementaux. Les projets sont menés par le Fablab Cohabit qui soutient et offre l’accès à ses services à un très large public (jeunes en situation de décrochage scolaire par exemple). Différents acteurs locaux sont également impliqués à savoir: Ossia-score, Végétal Signals. D’un point de vue environnemental, les contraintes sanitaires ont encouragé le travail à distance donc par voie informatique. Nous avons fonctionné étapes par étapes quand à l’impression des pièces par imprimante 3D, de façon à ne pas gaspiller de matières dans le cas d’erreurs.</p>
+				
+			</div>
+		</div>
 			<h1>Retours et Plan Vert</h1>
 		<div class="row">
 			<div class="col-lg-12 col-md-12 col-sm-12 contenu">
