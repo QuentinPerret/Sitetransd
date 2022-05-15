fb00e0e7f253d16f59175918a2bfd5666949aefb
QuentinPerret <quentinprrt33@gmail.com>
2022-05-15 17:33:38 +0200

    Pole info prograssion suit ok

--- poleinformatique.php
+++ poleinformatique.php
@@ -7,91 +7,52 @@
     ?>
 
     <body style="background-color: beige">
-      <div class="container">
-        <?php require_once "includes/header.php"; ?>
+      	<div class="container">
+        	<?php require_once "includes/header.php"; ?>
+			<div class="container-fluid">
+				<h1>Travail réalisé</h1>
+				<div class="row">
+					<div class="col-lg-12 col-md-12 col-sm-12 contenu">
+						<h3>Progression du projet</h3>
+						<p><b>Première partie : reprise du projet</b>
+						</p><ul>
+							<li>Le travail du pole informatique en octobre a commencé par une reprise du projet et des différents scripts laissé sur le wiki par le groupe d'étudiants de l'année précédente. Deux scripts ont été laissée : un en C et un en python. Les membres du pôle ayant déja fait du python. Nous avons décidé de nous concentré d'abord sur le scripts python lors de notre travail de reprise.</li>
+							<li>Les scripts laissées permettaient de transformer des valeurs héxadécimales contenus dans un fichier en des valuers décimales. C'est scripts ont pour objectif de transformé les valeurs envoyés par le capteur Végétal Signal afin de rendre leur traitement plus facile par la suite.</li>
+							<li>Néanmoins le code était très peu commenté et donc peu compréhensible, dans un soucis de partage nous avons décidé de le commenter plus précisement et de le documenté sur notre page wiki.</li>
+						</ul>
+						<b>Deuxème partie : traitement du signal</b>
+						<ul>
+							<li>Une fois le scrtipt python documenté nous avons souhaité ajouter au script python une partie de traitement du signal obtenu en étudiant sur moyenne au cours du temps mais aussi en décompant le sigal en série de Fourier.</li>
+							<li>Une rencontre avec les ingénieurs de Vegetal Signals nous a permis d'en apprendre plus sur les possibilités de traitement de signal avec leur capteur mais aussi leurs conseils nous ont permis de recaadrer notre travail.</li>
+						</ul>
+						<b>Troisième partie : changement de language de code </b>
+						<ul>
+							<li>Le pyhton avait été choisis car c'était un language déjà utilisé par les membres du pôle informatique mais celui-ci ne permettait pas de réaliser les opérations nécessaires de traitement des valeurs assez vite.</li>
+							<li>Nous avons donc décider de coder à prtésent en C en reprenant le script de transformation de valuer héxadécimales vers décimales.</li>
+							<li>Le C étant un language nouveau pour tout les membres du groupe, une période de formation a été nécessaire ce qui nous a retardé.</li>
+						</ul>
+						<p></p>
+						
+					</div>
+				</div>
 
-        <div class="container-fluid">
-		<h1>Travail réalisé ghjhcvbhgfvbh</h1>
-		<div class="row">
-			<div class="col-lg-12 col-md-12 col-sm-12 contenu">
-				<h3>Progression du projet</h3>
-				<p><b>Première partie : reprise du projet</b>
-				</p><ul>
-					<li>Le travail du pole informatique en octobre a commencé par une reprise du projet et des différents scripts laissé sur le wiki par le groupe d'étudiants de l'année précédente. Deux scripts ont été laissée : un en C et un en python. Les membres du pôle ayant déja fait du python. Nous avons décidé de nous concentré d'abord sur le scripts python lors de notre travail de reprise.</li>
-					<li>Les scripts laissées permettaient de transformer des valeurs héxadécimales contenus dans un fichier en des valuers décimales. C'est scripts ont pour objectif de transformé les valeurs envoyés par le capteur Végétal Signal afin de rendre leur traitement plus facile par la suite.</li>
-					<li>Néanmoins le code était très peu commenté et donc peu compréhensible, dans un soucis de partage nous avons décidé de le commenter plus précisement et de le documenté sur notre page wiki.</li>
-				</ul>
-				<b>Deuxème partie : traitement du signal</b>azeukjgbeimearnv
-				<ul>
-					<li>Une fois le scrtipt python documenté nous avons souhaité ajouter au script python une partie de traitement du signal obtenu en étudiant sur moyenne au cours du temps mais aussi en décompant le sigal en série de Fourier.</li>
-					<li>Une rencontre avec les </li>
-					<li></li>
-				</ul>
-				<b>Troisième partie : travail </b>
-				<ul>
-					<li>Une répartition et une planification des tâches de chacun en fonction de ses préférences et/ou compétences sur les deux projets.</li>
-					<li>Une meilleure mise en place du travail à domicile.</li>
-					<li>Nécessite un certain nombre de connaissances de bases, bien que nous ayons tout de même découvert de nombreux domaines.</li>
-					<li>Groupe mixte (ex : ENSC + Licence plus orientée informatique)</li>
-				</ul>
-				<p></p>
-				
+				<h1>Suite du projet</h1>
+
+				<div class="row">
+					<div class="col-lg-12 col-md-12 col-sm-12 contenu">
+						<h3>Travail restant</h3>
+						<ul>
+							<li>Finir les scripts C permettant de traiter les valurs renvoyé par le capteurs Vegetal Signals.</li>
+							<li>Réalisé des expériences afin de déterminer une loi permettant de diriger efficacement le robot vers le soleil.</li>
+							<li>Documenter le travail réalisé.</li>
+						</ul>
+						<p>Ce travail sera poursuivi par Quentin qui effectura son stage d'innitaition au fablab Cohabit.</p>
+						<p>L'ensemble des scripts écrits est disponible sur le <a href='https://git.cohabit.fr/pgp/Traitement-signal-plantes' target="_blank">GitTea</a> du projet </p>
+					</div>
+				</div>
 			</div>
 		</div>
-			<h1>Retours et Plan Vert</h1>
-		<div class="row">
-			<div class="col-lg-12 col-md-12 col-sm-12 contenu">
-				<h3>Rétrospective sur le projet</h3>
-				<p><b>Points forts</b>
-				</p><ul>
-					<li>Nous avons eu l’occasion d’acquérir des compétences dans des domaines qui nous étaient inconnus.</li>
-					<li>Prendre conscience des enjeux de la programmation de bas niveau.</li>
-					<li>La bonne entente au sein de l’équipe et des membres du Fablab.</li>
-					<li>Un cadre de travail bien équipé.</li>
-				</ul>
-				<b>Points faibles</b>
-				<ul>
-					<li>Nous n’avons pas eu l’occasion de relever des signaux provenant des plantes.</li>
-					<li>Nos objectifs étaient peut être trop ambitieux, nous n’avons donc pas réussi à les atteindre.</li>
-					<li>Le fait que le cadre de travail soit bien équipé implique également une forte dépendance au fablab pour travailler. Ce qui fut compliqué avec les conditions sanitaires.</li>
-				</ul>
-				<b>Pistes d'amélioration</b>
-				<ul>
-					<li>Une répartition et une planification des tâches de chacun en fonction de ses préférences et/ou compétences sur les deux projets.</li>
-					<li>Une meilleure mise en place du travail à domicile.</li>
-					<li>Nécessite un certain nombre de connaissances de bases, bien que nous ayons tout de même découvert de nombreux domaines.</li>
-					<li>Groupe mixte (ex : ENSC + Licence plus orientée informatique)</li>
-				</ul>
-				<p></p>
-				
-				<h3>Retours personnels</h3>
-				<ul><li><b>Romain : </b>Ce projet a permis à l’ensemble des membres du groupe de toucher à de nombreux domaines et à diversifier leur panel de compétences, et à permis de nous ouvrir à de nouveaux domaines. L’ensemble de l’équipe du Fablab a été d’une grande compréhension et patience, ce qui a accentué notre envie et notre plaisir à poursuivre le projet chaque semaine. Nous remercions plus particulièrement Pierre Grangé-Praderas, pour nous avoir transmis son savoir et nous avoir aidé dans la progression du projet.</li>
-				<li><b>Robin : </b>Ce projet transdisciplinaire a été l’occasion pour moi de m'enrichir autant d’un point de vue pédagogique que social. La complexité du projet au vu de mes anciennes années d’étude m’a permis de me former sur des sujets encore inconnus mais très intéressants: impression 3D, programmation Bash… A cette occasion j’ai également eu la chance de découvrir une structure comme le Fablab Coh@bit composée de personnes attentionnées prêtes à vous aider à n’importe quel moment. Le rendu de fin d’année n’est malheureusement pas à la hauteur de nos attentes, cependant nous avons apporté notre pierre à l’édifice quant à la progression de notre robot vers le soleil.</li>
-				<li><b>Brandon : </b>Au cours de ce projet, nous avons pu apprendre beaucoup de choses dans des domaines très variés (modélisation 3D avec FreeCad, impression 3D, commandes BASH, …). Ce fut donc une expérience très enrichissante. De surcroît, l’ambiance au sein du groupe était très agréable, de même qu’avec les divers membres du Fablab avec lesquels nous avons pu travailler. Leur pédagogie nous a permis d’avancer et de faire évoluer le projet malgré la présence de disciplines qui nous étaient inconnues. Les acquis pédagogiques furent donc majeurs. J’aurai néanmoins aimé avoir l’occasion d’avancer un petit peu plus sur ce projet, néanmoins les conditions sanitaires et la difficulté de la plupart des tâches nous ont beaucoup ralenties. Enfin, faisant moi même assez régulièrement des projets manuels sur mon temps libre, un lieu regorgeant d’outils comme le Fablab était naturellement une agréable découverte. </li>
-				<li><b>Lucas : </b>Pour moi le projet transdisciplinaire fut une expérience agréable. Il m’a permis de découvrir des structures différentes de celles auxquelles j’étais habitué. En effet, le Fablab est un lieu de partage de connaissances et d’entraide sur les projets qui est fort intéressant. Nous avons pu échanger avec des personnes de divers horizons, ce qui était enrichissant. Au niveau du projet en lui-même, j’ai acquis des nouvelles compétences et je me suis également rendu compte de l’importance de la méthodologie et de l’organisation du travail en groupe sur des projets conséquents et impliquant des acteurs extérieurs. </li></ul>
-				
-				<h3>Plan Vert</h3>
-				<p>Le Plan Vert doit recouvrir toutes les dimensions du développement durable c'est-à-dire par des aspects économiques, sociaux et environnementaux. Les projets sont menés par le Fablab Cohabit qui soutient et offre l’accès à ses services à un très large public (jeunes en situation de décrochage scolaire par exemple). Différents acteurs locaux sont également impliqués à savoir: Ossia-score, Végétal Signals. D’un point de vue environnemental, les contraintes sanitaires ont encouragé le travail à distance donc par voie informatique. Nous avons fonctionné étapes par étapes quand à l’impression des pièces par imprimante 3D, de façon à ne pas gaspiller de matières dans le cas d’erreurs.</p>
-				
-			</div>
-		</div>
-
-		</div>
-
-          <?php require_once "includes/footer.php"; ?>
-      </div>
-
-      <?php require_once "includes/scripts.php"; ?>
+		<?php require_once "includes/footer.php"; ?>
+		<?php require_once "includes/scripts.php"; ?>
     </body>
-
-  </html>
-
-
-    <html>
-
-    <body>
-      <img src="https://sd.keepcalm-o-matic.co.uk/i/don-t-mess-with-me-bro.png" />
-    </body>
-
-    </html>
-    
+  </html>    
