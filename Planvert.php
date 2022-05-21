e424247bdf0a097d74b40ff33d1ba023670fa87a
QuentinPerret <quentinprrt33@gmail.com>
2022-05-21 19:01:54 +0200

    modifie image plan vert

--- Planvert.php
+++ Planvert.php
@@ -19,9 +19,12 @@
 				<p>Le Plan Vert doit recouvrir toutes les dimensions du Développement Durable et de la Responsabilité Sociétale (DD&RS), c'est-à-dire par des aspects économiques, sociaux et environnementaux.</p>
 				<p>Le projet est mené par le Fablab Cohabit qui soutient et offre l’accès à ses services à un très large public, et accueille notamment des jeunes en situation de décrochage scolaire par exemple.</p>
 				<p>Différents acteurs locaux sont également impliqués et notamment <strong>Végétal Signals</strong>.</p>
-				<p>D’un point de vue environnemental, les débris de plastique générés lors des impressions 3D ainsi que les pièces à changer étaient recyclés, et nous ne ré-imprimions que les pièces qui ne pouvaient vraiment pas être utilisées en l'état (nous limions certaines pièces qui n'avaient pas besoin d'être forcément imprimées de nouveau après la modification des plans par exemple).</p>
 				<p>Pour le test sur le framboisier que nous avons réalisé, nous avions ramener un framboisier qui venait de chez nous</p>
-				<img src="images/recyclage.jpg" alt="oeuvre en pla recyclée" width=200/>	
+				<p>D’un point de vue environnemental, les débris de plastique générés lors des impressions 3D ainsi que les pièces à changer étaient recyclés, et nous ne ré-imprimions que les pièces qui ne pouvaient vraiment pas être utilisées en l'état (nous limions certaines pièces qui n'avaient pas besoin d'être forcément imprimées de nouveau après la modification des plans par exemple).</p>
+				<div class="centre">
+					<img src="images/recyclage.jpg" alt="oeuvre en pla recyclée" height="200px"/><br/>
+					<u>Oeuvre en pla recyclée</u>
+				</div>
 			</div>
 		</div>
 
