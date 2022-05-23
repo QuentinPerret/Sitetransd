82dbdcb250bc09cebbbbb10e21e6e7882571be13
Quentin Perret <quentinprrt33@gmail.com>
2022-05-23 14:41:46 +0200

    add para

--- Documents.php
+++ Documents.php
@@ -18,10 +18,10 @@
     <div class="col-lg-12 col-md-12 col-sm-12 contenu">
       <h2>Documentation</h2>
       <p>Vous pouvez accéder au wiki du Fablab Coh@bit et plus spécialement à la documentation relatif à notre projet <a href="https://projets.cohabit.fr/redmine/projects/communication-racinaire/wiki">ici</a>. Nous avons travaillé sur l'expérience 2 , Robot Go-West durant cette année. Le reste ne fut pas réalisé par notre équipe.</p>
-      <p>L'ensemble des scripts écrits est disponible sur le <a href='https://git.cohabit.fr/pgp/Traitement-signal-plantes' target="_blank">GitTea</a> du projet.<br/>
+      <p>L'ensemble des scripts écrits est disponible sur le <a href='https://git.cohabit.fr/pgp/Traitement-signal-plantes' target="_blank">GitTea</a> du projet.</p>
       <p><b>Autres documents</b></p>
       
-      <a href="pdf/CdC V2.pdf" target="_blank">Cahier des Charges version finale</a><br/>
+      <p><a href="pdf/CdC V2.pdf" target="_blank">Cahier des Charges version finale</a><br/>
       <a href="pdf/Matrice d'implication.pdf" target="_blank">Matrice d'implication</a><br/>
       <a href="pdf/Questionnaire PTRD-UE Gestion de Projet.pdf" target="_blank">Questionnaire PTRD-UE Gestion de Projet</a><br/></p>
       <img class="illustration" src="images/chat.jpg" alt="Chat" height="400">	
